00132e707b002138781eeb1f411523832855fd10
PerlaNavaSof <perla.nava@softtek.com>
2023-03-22 15:53:36 -0600

    test2

---
+++ learndbt/models/example/cumulative_orders_by_date.sql
@@ -0,0 +1,9 @@
+{{ config(materialized='table') }}
+
+select
+    O_ORDERDATE,  
+    sum(O_TOTALPRICE) as comulative,
+    sum(comulative) over (order by O_ORDERDATE) results_cumulative
+from "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1"."ORDERS"
+group by O_ORDERDATE
+ORDER BY O_ORDERDATE asc

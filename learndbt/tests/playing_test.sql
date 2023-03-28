7dee6973c472ed4e54d658bfa8b4661a97d89829
PerlaNavaSof <perla.nava@softtek.com>
2023-03-27 19:20:48 -0600

    fixed test

--- learndbt/tests/playing_test.sql
+++ learndbt/tests/playing_test.sql
@@ -1,4 +1,4 @@
 select sum(case when C_CUSTKEY is null then 1 else 0 end) / count(C_ACCTBAL) as total
 from {{ref('playing_with_test')}}
 where C_MKTSEGMENT in ('BUILDING', 'MACHINERY', 'AUTOMOBILE', 'HOUSEHOLD', 'FURNITURE')
-having sum(case when C_CUSTKEY is null then 1 else 0 end) / count(C_ACCTBAL) < 100000000
+having sum(case when C_CUSTKEY is null then 1 else 0 end) / count(C_ACCTBAL) = 674326849.74
